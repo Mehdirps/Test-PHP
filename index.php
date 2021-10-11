e93ac31c3527514d23c9bc57ae075374e2712e9d
Mehdirps <Mehdi.raposo77@gmail.com>
2021-10-11 10:15:26 +0200

    petit test PHP

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
 
-echo "<h1>Salut</h1>";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test PHP</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php
+
+    $prenom = "Mehdi";
+    $reponse = "Tu veux un café";
+    echo "<h1>Salut $prenom, comment ca va ? $reponse !</h1>";
+    ?>
+    
+</body>
+
+</html>
